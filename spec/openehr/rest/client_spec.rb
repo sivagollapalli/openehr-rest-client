74da85c1df05391642d2e0f14d6e80b72be3f103
Siva Gollapalli <sivagollapalli88@gmail.com>
2023-12-12 19:08:01 +0530

    Initial commit

---
+++ spec/openehr/rest/client_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Openehr::Rest::Client do
+  it "has a version number" do
+    expect(Openehr::Rest::Client::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
